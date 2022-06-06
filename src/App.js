87f21e3e635c8e747837446714941bcec45caf11
[SW2/서울/1반] 최두웅 <justicejustice314@gmail.com>
2022-06-06 14:52:12 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+function HeaderTag(){
+  return <header><h1><a href="/">html</a></h1></header>
+}
+function Nav(props){
+  console.log("Props",props.data);
+  const list= props.data.map(function(e){
+    return <li key={e.id}><a href={'/read/'+e.id}>{e.title}</a></li>
+  })
+  //li 의 key property 는 react convenction
+  // [ <li><a href="/read1">html</a></li>
+  // ,<li><a href="/read2">css</a></li>];
+
+  return  <nav><ol>
+     {list}
+  </ol>
+</nav>
+}
+function Article(props){
+  return   <article>
+  <h2>{props.title}</h2>
+  {props.body}
+  </article>
+}
 function App() {
+  const topics=[ 
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <HeaderTag></HeaderTag>
+       <Nav data={topics} ></Nav>
+      <Article title="HTML" body="HTML is ..."></Article>
+      <Article title="CSS" body="CSS is ..."></Article>
+       
     </div>
   );
 }
+
 
 export default App;
